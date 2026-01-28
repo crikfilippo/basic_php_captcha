ba4073faf5b49b5e30a6732cfd6c9dd8b7f7d30a
Filippo Maria Grilli <93493236+crikfilippo@users.noreply.github.com>
2026-01-28 11:12:28 +0100

    Update GitHub link in Captcha.js

--- Captcha.js
+++ Captcha.js
@@ -10,7 +10,7 @@ class Captcha{
 	//@version 1.0.0
 	//@since 2026-01-15
 	//@license MIT
-	//@link https://github.com/crikfilippo/basic_php_captcha
+	//@link https://github.com/crikfilippo/basic_captcha
 	//
 	//
 	//-----------------
